f0728ab3a64768ca26cdf50ead0f294941471995
eugenetedkim <eugenetedkim@gmail.com>
2023-01-21 00:34:10 -0800

    Change port to 5000

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = require('./app');
 
 // Assign a port where our project should run
 // e.g. localhost:3000
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 // Create the server and pass a request listener function that is executed each time the server get a request.
 const server = http.createServer(app);
 
 server.listen(port);
+
+console.log(`Server listening on port ${port}`);
