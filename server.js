49bc2e63506adb4f7af67af4abdc2982021966cf
eugenetedkim <eugenetedkim@gmail.com>
2023-01-29 10:05:18 -0800

    Removed .env and changed port numbers

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 // Assign a port where our project should run
 // e.g. localhost:3000
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3501;
 
 // Create the server and pass a request listener function that is executed each time the server get a request.
 const server = http.createServer(app);
